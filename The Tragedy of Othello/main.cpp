812f5e19e8bc6284bf12067775759ccd3bd3335c
David Van <David@DavidVan.net>
2015-02-26 01:06:04 -0800

    Moved main to its own file.

---
+++ The Tragedy of Othello/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "OthelloGame.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+   int row = 0, col = 0, currentPlayer = 1;
+   bool playing = true, bPass = false, wPass = false;
+
+   char board[BOARD_SIZE][BOARD_SIZE] = {
+      {0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, -1, 1, 0, 0, 0},
+      {0, 0, 0, 1, -1, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+   };
+   cout << "Welcome to Othello!" << endl;
+   PrintBoard(board);
+   while (playing) {
+      if (currentPlayer == 1) {
+         cout << "Black's turn. Please choose a move: " << endl;
+         GetMove(&row, &col);
+      }
+      else if (currentPlayer == -1) {
+         cout << "White's turn. Please choose a move: " << endl;
+         GetMove(&row, &col);
+      }
+      while (!IsValidMove(board, row, col)) {
+         cout << "Invalid Move. Please choose a move: " << endl;
+         GetMove(&row, &col);
+      }
+      if (row == -1 && col == -1) {
+         if (currentPlayer == 1) {
+            bPass = true;
+         }
+         else if (currentPlayer == -1) {
+            wPass = true;
+         }
+      }
+      else {
+         ApplyMove(board, row, col, currentPlayer);
+      }
+      if (bPass && wPass) {
+         if (GetValue(board) > 0) {
+            cout << "\nGame over. Black wins!" << endl;
+         }
+         else if (GetValue(board) < 0) {
+            cout << "\nGame over. White wins!" << endl;
+         }
+         else if (GetValue(board) == 0) {
+            cout << "\nGame over. We have a tie!" << endl;
+         }
+         playing = false;
+      }
+      else {
+         PrintBoard(board);
+      }
+      if (row != -1 && col != -1 && ((bPass && !wPass) || (!bPass && wPass))) {
+         cout << "Reset" << endl;
+         bPass = false, wPass = false;
+      }
+      currentPlayer = currentPlayer == 1 ? -1 : 1;
+   }
+}
