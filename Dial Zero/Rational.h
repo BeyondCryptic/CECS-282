4602af9584c83e41b2762db02a67dabae938ac9a
David Van <David@DavidVan.net>
2015-03-16 14:10:07 -0700

    Started new project.

---
+++ Dial Zero/Rational.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#ifndef __RATIONAL_H
+#define __RATIONAL_H
+
+class Rational {
+private:
+   int mNumerator;
+   int mDenominator;
+   void Normalize();
+   const int gcd(int numberOne, int numberTwo);
+public:
+   Rational(const int &numerator, const int &denominator);
+   Rational();
+   Rational(const Rational &copyThis);
+   const int GetNumerator() const;
+   const int GetDenominator() const;
+   void SetNumerator(int numerator);
+   void SetDenominator(int denominator);
+   bool Equals(const Rational &other) const;
+   Rational Add(const Rational &other) const;
+   const std::string ToString() const;
+   friend std::ostream &operator<<(std::ostream &lhs, const Rational &rhs);
+   friend const Rational &friend const Rational operator=(const Rational &lhs, const Rational &rhs);operator=(const Rational &lhs, const Rational &rhs);
+};
+
+#endif
