edc3ba12cc5aa1b28b563e262ba79d80189e590e
David Van <David@DavidVan.net>
2015-03-23 16:25:11 -0700

    New project. Finished, I think.

---
+++ Array, a Drop of Golden Sun/Rational.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <string>
+#include "Rational.h"
+
+using namespace std;
+
+Rational::Rational(const int &numerator, const int &denominator)
+   :  mNumerator(numerator), mDenominator(denominator)
+{
+   Normalize();
+}
+
+Rational::Rational()
+   :  mNumerator(0), mDenominator(1)
+{
+   Normalize();
+}
+
+Rational::Rational(const Rational &copyThis)
+{
+   mNumerator = copyThis.mNumerator;
+   mDenominator = copyThis.mDenominator;
+}
+
+const int Rational::GetNumerator() const {
+   return mNumerator;
+}
+
+const int Rational::GetDenominator() const {
+   return mDenominator;
+}
+
+void Rational::SetNumerator(int numerator) {
+   mNumerator = numerator;
+   Normalize();
+}
+
+void Rational::SetDenominator(int denominator) {
+   mDenominator = denominator;
+   Normalize();
+}
+
+bool Rational::Equals(const Rational &other) const {
+   if (mNumerator == other.mNumerator && mDenominator == other.mDenominator) {
+      return true;
+   }
+   else {
+      return false;
+   }
+}
+
+Rational Rational::Add(const Rational &other) const {
+   int numerator = mDenominator * other.mNumerator
+      + other.mDenominator * mNumerator;
+   int denominator = mDenominator * other.mDenominator;
+   return Rational(numerator, denominator);
+}
+
+const string Rational::ToString() const {
+   string rationalNumber;
+   if (mDenominator == 0 || mDenominator == 1) {
+      return rationalNumber = to_string(mNumerator);
+   }
+   else {
+      return rationalNumber = to_string(mNumerator) + "/"
+         + to_string(mDenominator);
+   }
+}
+
+void Rational::Normalize() {
+   if (mDenominator < 0 && (mNumerator > 0 || mNumerator < 0)) {
+      mNumerator *= -1;
+      mDenominator *= -1;
+   }
+   int scalar = gcd(mNumerator, mDenominator);
+   if (scalar < 0) {
+      scalar *= -1;
+   }
+   mNumerator /= scalar;
+   mDenominator /= scalar;
+}
+
+const int Rational::gcd(int numberOne, int numberTwo) {
+   while (numberTwo != 0) {
+      int remainder = numberOne % numberTwo;
+      numberOne = numberTwo;
+      numberTwo = remainder;
+   }
+   return numberOne;
+}
+
+ostream &operator<<(std::ostream &lhs, const Rational &rhs) {
+   lhs << rhs.ToString() << endl;
+   return lhs;
+}
+
+Rational& Rational::operator=(const Rational &rhs) {
+   mNumerator = rhs.mNumerator;
+   mDenominator = rhs.mDenominator;
+   return *this;
+}
+
+const Rational operator+(const Rational &lhs, const Rational &rhs) {
+   return lhs.Add(rhs);
+}
+
+const Rational operator-(const Rational &rhs) {
+   int numerator = rhs.mNumerator * -1;
+   int denominator = rhs.mDenominator;
+   return Rational(numerator, denominator);
+}
+
+const Rational operator-(const Rational &lhs, const Rational &rhs) {
+   int numerator = rhs.mDenominator * lhs.mNumerator
+         - lhs.mDenominator * rhs.mNumerator;
+   int denominator = lhs.mDenominator * rhs.mDenominator;
+   return Rational(numerator, denominator);
+}
+
+const Rational operator*(const Rational &lhs, const Rational &rhs) {
+   int numerator = lhs.mNumerator * rhs.mNumerator;
+   int denominator = lhs.mDenominator * rhs.mDenominator;
+   return Rational(numerator, denominator);
+}
+
+const Rational operator/(const Rational &lhs, const Rational &rhs) {
+   int numerator = lhs.mNumerator * rhs.mDenominator;
+   int denominator = lhs.mDenominator * rhs.mNumerator;
+   return Rational(numerator, denominator);
+}
+
+const bool operator==(const Rational &lhs, const Rational &rhs) {
+   return lhs.Equals(rhs);
+}
+
+const bool operator!=(const Rational &lhs, const Rational &rhs) {
+   return !(lhs==rhs);
+}
+
+const bool operator<(const Rational &lhs, const Rational &rhs) {
+   double first = lhs.mNumerator / lhs.mDenominator;
+   double second = rhs.mNumerator / rhs.mDenominator;
+   return first < second;
+}
+
+const bool operator>(const Rational &lhs, const Rational &rhs) {
+   double first = lhs.mNumerator / lhs.mDenominator;
+   double second = rhs.mNumerator / rhs.mDenominator;
+   return first > second;
+}
+
+const bool operator<=(const Rational &lhs, const Rational &rhs) {
+   double first = lhs.mNumerator / lhs.mDenominator;
+   double second = rhs.mNumerator / rhs.mDenominator;
+   return first <= second;
+}
+
+const bool operator>=(const Rational &lhs, const Rational &rhs) {
+   double first = lhs.mNumerator / lhs.mDenominator;
+   double second = rhs.mNumerator / rhs.mDenominator;
+   return first >= second;
+}
---
+++ Array, a Drop of Golden Sun/RationalArray.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include "Rational.h"
+#include "RationalArray.h"
+
+using namespace std;
+
+RationalArray::RationalArray(int size)
+   :  mSize(size), mArray(new Rational[size])
+{
+}
+
+RationalArray::~RationalArray() {
+   delete[] mArray;
+   mArray = nullptr;
+}
+
+RationalArray::RationalArray(const RationalArray &other)
+   :  mSize(other.Size()), mArray(new Rational[other.Size()])
+{
+   for (int i = 0; i < other.Size(); i++) {
+      mArray[i] = other[i];
+   }
+}
+
+RationalArray& RationalArray::operator=(const RationalArray& rhs) {
+   if (Size() == rhs.Size()) {
+      for (int i = 0; i < rhs.Size(); i++) {
+        mArray[i] = rhs[i];
+      }
+   }
+   else {
+      cout << "Deleting " << &mArray << endl;
+      delete[] mArray;
+      mArray = nullptr;
+      mSize = rhs.mSize;
+      cout << "Creating " << &mArray << endl;
+      mArray = new Rational[mSize];
+      for (int i = 0; i < rhs.Size(); i++) {
+         mArray[i] = rhs[i];
+      }
+   }
+   return *this;
+}
+
+Rational& RationalArray::operator[](int rhs) const {
+   if (rhs > Size()-1) {
+      throw out_of_range(": out of bounds!");
+   }
+   return mArray[rhs];
+}
